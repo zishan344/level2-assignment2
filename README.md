99cda2bf77cff61003ba7c75c45a39cee47e5ab6
zishan344 <zishanahmed344@gmail.com>
2023-11-23 18:00:13 +0600

    user interface created done

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # level2-assignment2
+
+# (api an point )https://level2-assignment2-zeta.vercel.app
---
+++ src/app/modules/users/user.interface.ts
@@ -0,0 +1,25 @@
+interface IUser {
+  userId: number;
+  username: string;
+  password: string;
+  fullName: {
+    firstName: string;
+    lastName: string;
+  };
+  age: number;
+  email: string;
+  isActive: boolean;
+  hobbies: string[];
+  address: {
+    street: string;
+    city: string;
+    country: string;
+  };
+  orders?: {
+    productName: string;
+    price: number;
+    quantity: number;
+  }[];
+}
+
+export default IUser;
